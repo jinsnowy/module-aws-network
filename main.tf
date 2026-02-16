776827cec748ea2a60dcca301cfef173da5777ac
GyeongMin Yoon <152632692+opettikemi5@users.noreply.github.com>
2026-02-17 00:22:50 +0900

    update route53 dns

--- main.tf
+++ main.tf
@@ -17,7 +17,10 @@ locals {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = var.main_vpc_cidr
+  cidr_block           = var.main_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  
   tags = {
     "Name"                                        = local.vpc_name
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
